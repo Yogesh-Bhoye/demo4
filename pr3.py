c809c6ffbe9a5a4d50c1e8c5d430561a966111cb
Yogesh-Bhoye <bhoyey840@gmail.com>
2025-04-27 16:28:59 +0530

    pr3.py

---
+++ pr3.py
@@ -0,0 +1,10 @@
+a=10
+b=3
+print('Addition of two number is:')
+print(a+b)
+print('Subtraction of two number is:')
+print(a-b)
+print('Multiplication of two number is:')
+print(a*b)
+print('Division of two number is:')
+print(a/b)
